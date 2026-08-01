4d8955a34e5de1be127dfe239553e2a3de14e8ac
so8380927-jpg <so8380927@gmail.com>
2026-08-01 13:13:22 +0300

    Update cache.js

--- js/cache.js
+++ js/cache.js
@@ -1,1 +1,21 @@
+const CACHE_DURATION = 24 * 60 * 60 * 1000;
 
+export function saveCache(key, data) {
+    const cacheObject = { timestamp: Date.now(), data: data };
+    localStorage.setItem(key, JSON.stringify(cacheObject));
+}
+
+export function getCache(key) {
+    const cachedString = localStorage.getItem(key);
+    if (!cachedString) return null;
+    try {
+        const cacheObject = JSON.parse(cachedString);
+        if (Date.now() - cacheObject.timestamp > CACHE_DURATION) {
+            localStorage.removeItem(key);
+            return null;
+        }
+        return cacheObject.data;
+    } catch (e) {
+        return null;
+    }
+}
